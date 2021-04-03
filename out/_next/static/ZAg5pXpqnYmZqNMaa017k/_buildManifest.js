64edac1123049ff39407da885c50ad3452b0189a
AntonyRafael <depaulacostaantony@gmail.com>
2021-04-03 14:05:13 -0300

    deploy

---
+++ out/_next/static/ZAg5pXpqnYmZqNMaa017k/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-fd57826ec96372e44b60.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d3fa1c8d372e54207d52.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
